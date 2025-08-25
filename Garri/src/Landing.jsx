ceaf3aadb6f0a797195fb4a2e4902f15e4d47fc3
Anakosile <114302133+Anakosile@users.noreply.github.com>
2025-08-25 15:36:29 +0100

    files

---
+++ Garri/src/Landing.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const LandingPage = () => {
+    return ( 
+        <>
+        <>welcome</>
+        <Outlet/>
+        </>
+     );
+}
+ 
+export default LandingPage;
--- Garri/src/main.jsx
+++ Garri/src/main.jsx
@@ -1,6 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LandingPage from './Landing'
+import Navbar from './nabar'
+
+export default function App () {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <LandingPage/>,
+      children: ([
+        {
+          path: 'details',
+          index: true
+        },
+        {
+          path: 'navbar',
+          element: <Navbar/>
+        }
+
+      ])
+    }
+  ])
+
+  return <RouterProvider router={router}/>
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
